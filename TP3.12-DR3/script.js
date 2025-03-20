5f88b91fafc0f032697762b6ea300c42614b958b
Lohran Mendes <lohranfm@gmail.com>
2025-03-20 14:51:41 -0300

    feat: solved questions 12 and 13

--- TP3.12-DR3/script.js
+++ TP3.12-DR3/script.js
@@ -6,3 +6,27 @@
 // Arrastar os elementos div de classe “caixinha” entre os elementos <div class='container'>
 // Link: https://codesandbox.io/p/sandbox/2n7w43
 // Observação: Faça as modificações necessárias no arquivo “index.html” para resolver o exercício.
+
+const containers = document.querySelectorAll(".container");
+const caixinha = document.querySelector(".caixinha");
+
+containers.forEach((container) => {
+  caixinha.addEventListener("drag", () => {
+    caixinha.style.backgroundColor = "rgba(255, 0, 0, 0.445)";
+  });
+  caixinha.addEventListener("dragend", () => {
+    caixinha.style.backgroundColor = "red";
+  });
+  container.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    container.style.backgroundColor = "rgb(0, 0, 122)";
+  });
+  container.addEventListener("dragleave", (event) => {
+    event.preventDefault();
+    container.style.backgroundColor = "blue";
+  });
+  container.addEventListener("drop", (event) => {
+    event.preventDefault();
+    container.appendChild(caixinha);
+  });
+});
--- TP3.13-DR3/script.js
+++ TP3.13-DR3/script.js
@@ -8,3 +8,60 @@
 // Permitir arrastar os elementos div de classe caixinha entre os elementos <div class='container'>.
 // Link: https://codesandbox.io/p/sandbox/r2p7ww
 // Observação: Faça as modificações necessárias no arquivo “index.html” para resolver o exercício.
+
+const containerDeCaixinha = document.querySelector(".container-de-caixinhas");
+const containers = document.querySelectorAll("#container > div");
+const cores = ["green", "purple", "orange", "red"];
+
+let caixinhas = document.querySelectorAll(".caixinha");
+let caixaCriada = 0;
+let caixaAtual;
+
+function adicionaEventosCaixinha() {
+  caixinhas.forEach((caixa) => {
+    caixa.addEventListener("drag", () => {
+      caixa.style.opacity = 0.4;
+      caixaAtual = caixa;
+    });
+    caixa.addEventListener("dragend", () => {
+      caixa.style.opacity = 1;
+    });
+  });
+}
+
+containers.forEach((container) => {
+  container.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    container.style.backgroundColor = "rgba(0, 0, 0, 0.15)";
+  });
+  container.addEventListener("dragleave", (event) => {
+    event.preventDefault();
+    container.style.backgroundColor = "transparent";
+  });
+  container.addEventListener("drop", (event) => {
+    event.preventDefault();
+    container.appendChild(caixaAtual);
+    container.style.backgroundColor = "transparent";
+  });
+});
+
+containerDeCaixinha.addEventListener("dragover", (event) => {
+  event.preventDefault();
+});
+
+containerDeCaixinha.addEventListener("drop", (event) => {
+  event.preventDefault();
+  containerDeCaixinha.appendChild(caixaAtual);
+});
+
+function novaCaixa() {
+  if (caixaCriada >= 4) caixaCriada = 0;
+  const caixaNova = document.createElement("div");
+  caixaNova.classList.add("caixinha");
+  caixaNova.draggable = true;
+  caixaNova.style.backgroundColor = cores[caixaCriada++];
+  containerDeCaixinha.appendChild(caixaNova);
+  caixinhas = document.querySelectorAll(".caixinha");
+  adicionaEventosCaixinha();
+}
+adicionaEventosCaixinha();
